fc3caa269f8900edc4ca4f00fdbb46586bc95381
Alain Royce Creencia <alain_creencia@dlsu.edu.ph>
2021-08-27 23:45:48 +0800

    bldgs

--- Creencia_Alain_GDGRAPHX/Source.cpp
+++ Creencia_Alain_GDGRAPHX/Source.cpp
@@ -119,6 +119,52 @@ int main() {
 		1.0f,
 		moonOffsets
 	);
+	ObjData barn;
+	//backpack.textures = 
+	LoadObjFile(&barn, "Powerplant/10078_Nuclear_Power_Plant_v1_L3.obj");
+	GLfloat barnOffsets[] = { 0.0f, 0.0f, 0.0f };
+	LoadObjToMemory(
+		&barn,
+		1.0f,
+		barnOffsets
+	);
+
+	ObjData structure;
+	//backpack.textures = 
+	LoadObjFile(&structure, "structure/10063_Church_v3.obj");
+	GLfloat structureOffsets[] = { 0.0f, 0.0f, 0.0f };
+	LoadObjToMemory(
+		&structure,
+		1.0f,
+		structureOffsets
+	);
+	ObjData structure2;
+	//backpack.textures = 
+	LoadObjFile(&structure2, "structure2/13943_Flatiron_Building_v1_l1.obj");
+	GLfloat structure2Offsets[] = { 0.0f, 0.0f, 0.0f };
+	LoadObjToMemory(
+		&structure2,
+		1.0f,
+		structure2Offsets
+	);
+	ObjData structure3;
+	//backpack.textures = 
+	LoadObjFile(&structure3, "structure3/10060_Big_Box_Storefront_V4_L3.obj");
+	GLfloat structure3Offsets[] = { 0.0f, 0.0f, 0.0f };
+	LoadObjToMemory(
+		&structure3,
+		1.0f,
+		structure3Offsets
+	);
+	ObjData structure4;
+	//backpack.textures = 
+	LoadObjFile(&structure4, "structure4/10093_Wembley_stadion_V3_Iteration0.obj");
+	GLfloat structure4Offsets[] = { 0.0f, 0.0f, 0.0f };
+	LoadObjToMemory(
+		&structure4,
+		1.0f,
+		structure4Offsets
+	);
 
 	/*
 	ObjData sun;
@@ -173,6 +219,18 @@ int main() {
 	glUniformMatrix4fv(modelTransformLoc, 1, GL_FALSE, glm::value_ptr(trans1));
 	glm::mat4 trans2 = glm::mat4(1.0f); // identity
 	glUniformMatrix4fv(modelTransformLoc, 1, GL_FALSE, glm::value_ptr(trans2));
+	glm::mat4 trans3 = glm::mat4(1.0f); // identity
+	glUniformMatrix4fv(modelTransformLoc, 1, GL_FALSE, glm::value_ptr(trans3));
+	glm::mat4 trans4 = glm::mat4(1.0f); // identity
+	glUniformMatrix4fv(modelTransformLoc, 1, GL_FALSE, glm::value_ptr(trans4));
+	glm::mat4 trans5 = glm::mat4(1.0f); // identity
+	glUniformMatrix4fv(modelTransformLoc, 1, GL_FALSE, glm::value_ptr(trans5));
+	glm::mat4 trans6 = glm::mat4(1.0f); // identity
+	glUniformMatrix4fv(modelTransformLoc, 1, GL_FALSE, glm::value_ptr(trans6));
+	glm::mat4 trans7 = glm::mat4(1.0f); // identity
+	glUniformMatrix4fv(modelTransformLoc, 1, GL_FALSE, glm::value_ptr(trans7));
+
+
 
 	// define projection matrix
 	glm::mat4 projection = glm::mat4(1.0f);
@@ -269,7 +327,7 @@ int main() {
 		glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));*/
 		glm::mat4 view = glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp);
 
-		float cameraSpeed = deltaTime;
+		float cameraSpeed = deltaTime*100;
 		if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS) {
 			cameraPos += cameraSpeed * cameraFront;
 		}
@@ -341,7 +399,7 @@ int main() {
 		trans1 = glm::rotate(trans, glm::radians(270.0f), glm::vec3(1.0f, 0.0f, 0.0f));
 		//trans1 = glm::rotate(trans, glm::radians(270.0f), glm::vec3(0.0f, 1.0f, 0.0f));
 
-		trans1 = glm::translate(trans1, glm::vec3(0.0f, 0.0f, -100.0f)); // matrix * translate_matrix
+		trans1 = glm::translate(trans1, glm::vec3(40.0f, 200.0f, -101.0f)); // matrix * translate_matrix
 		trans1 = glm::scale(trans1, glm::vec3(1.0f, 1.0f, 1.0f));
 		
 		//send to shader
@@ -363,6 +421,42 @@ int main() {
 
 		//unbindtexture after rendering
 		glBindTexture(GL_TEXTURE_2D, 0);
+
+
+
+		//////////////2
+		glBindVertexArray(earth.vaoId);
+		glUseProgram(shaderProgram); //changes
+
+		// transforms
+		trans7 = glm::mat4(1.0f); // identity
+		//rotates it to the origin point in the world where the sun is also positioned; revolves around the Sun
+		//trans1 = glm::rotate(trans1, glm::radians(xFactor2), glm::vec3(0.0f, 1.0f, 0.0f)); // matrix * rotation_matrix
+		trans7 = glm::rotate(trans, glm::radians(270.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+		//trans1 = glm::rotate(trans, glm::radians(270.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+
+		trans7 = glm::translate(trans7, glm::vec3(40.0f, 0.0f, -101.0f)); // matrix * translate_matrix
+		trans7 = glm::scale(trans7, glm::vec3(2.5f, 2.5f, 1.0f));
+
+		//send to shader
+		glm::mat4 normalTrans7 = glm::transpose(glm::inverse(trans7)); //changes
+		glUniformMatrix4fv(normalTransformLoc, 1, GL_FALSE, glm::value_ptr(normalTrans7)); //changes
+		glUniformMatrix4fv(modelTransformLoc, 1, GL_FALSE, glm::value_ptr(trans7));
+
+		glActiveTexture(GL_TEXTURE0);
+		GLuint earth2Texture = earth.textures[earth.materials[0].diffuse_texname];
+		glBindTexture(GL_TEXTURE_2D, earth2Texture);
+
+		// incerement rotation by deltaTime
+		currentTime = glfwGetTime();
+		deltaTime = currentTime - prevTime;
+		xFactor2 += deltaTime * xSpeed2;
+
+		//draw earth
+		glDrawElements(GL_TRIANGLES, earth.numFaces, GL_UNSIGNED_INT, (void*)0);
+
+		//unbindtexture after rendering
+		glBindTexture(GL_TEXTURE_2D, 0);
 		
 		////////// MOON
 		glBindVertexArray(moon.vaoId);
@@ -372,7 +466,7 @@ int main() {
 		trans = glm::mat4(1.0f); // identity
 		//rotates it to the origin point of the Earth; revolves around the Earth
 		//trans = glm::rotate(trans1, glm::radians(xFactor1), glm::vec3(0.0f, 1.0f, 0.0f)); // matrix * rotation_matrix
-		trans = glm::translate(trans, glm::vec3(0.0f, 0.0f, 0.0f)); // matrix * translate_matrix
+		trans = glm::translate(trans, glm::vec3(0.0f, 0.0f, 100.0f)); // matrix * translate_matrix
 		trans = glm::scale(trans, glm::vec3(0.4f, 0.4f, 0.4f));
 		//send to shader
 		glm::mat4 normalTrans = glm::transpose(glm::inverse(trans)); //changes
@@ -397,6 +491,197 @@ int main() {
 		//unbindtexture after rendering
 		glBindTexture(GL_TEXTURE_2D, 0);
 
+
+
+
+		//////////////Powerplant
+		glBindVertexArray(barn.vaoId);
+		glUseProgram(shaderProgram); //changes
+
+		// transforms
+		trans2 = glm::mat4(1.0f); // identity
+		//rotates it to the origin point in the world where the sun is also positioned; revolves around the Sun
+		//trans1 = glm::rotate(trans1, glm::radians(xFactor2), glm::vec3(0.0f, 1.0f, 0.0f)); // matrix * rotation_matrix
+		trans2 = glm::rotate(trans, glm::radians(270.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+		//trans1 = glm::rotate(trans, glm::radians(270.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+
+		trans2 = glm::translate(trans2, glm::vec3(0.0f, 0.0f, -90.0f)); // matrix * translate_matrix
+		trans2 = glm::scale(trans2, glm::vec3(0.5f, 0.5f, 0.5f));
+
+		//send to shader
+		glm::mat4 normalTrans2 = glm::transpose(glm::inverse(trans2)); //changes
+		glUniformMatrix4fv(normalTransformLoc, 1, GL_FALSE, glm::value_ptr(normalTrans2)); //changes
+		glUniformMatrix4fv(modelTransformLoc, 1, GL_FALSE, glm::value_ptr(trans2));
+
+		glActiveTexture(GL_TEXTURE0);
+		GLuint barnTexture =barn.textures[barn.materials[0].diffuse_texname];
+		glBindTexture(GL_TEXTURE_2D, barnTexture);
+
+
+		// incerement rotation by deltaTime
+		currentTime = glfwGetTime();
+		deltaTime = currentTime - prevTime;
+		xFactor2 += deltaTime * xSpeed2;
+
+		//draw earth
+		glDrawElements(GL_TRIANGLES, barn.numFaces, GL_UNSIGNED_INT, (void*)0);
+
+		//unbindtexture after rendering
+		glBindTexture(GL_TEXTURE_2D, 0);
+
+
+
+		//////////////Church
+		glBindVertexArray(structure.vaoId);
+		glUseProgram(shaderProgram); //changes
+
+		// transforms
+		trans3 = glm::mat4(1.0f); // identity
+		//rotates it to the origin point in the world where the sun is also positioned; revolves around the Sun
+		//trans1 = glm::rotate(trans1, glm::radians(xFactor2), glm::vec3(0.0f, 1.0f, 0.0f)); // matrix * rotation_matrix
+		trans3 = glm::rotate(trans, glm::radians(270.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+		//trans1 = glm::rotate(trans, glm::radians(270.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+
+		trans3 = glm::translate(trans3, glm::vec3(120.0f, 200.0f, -90.0f)); // matrix * translate_matrix
+		trans3 = glm::scale(trans3, glm::vec3(0.03f, 0.03f, 0.03f));
+
+		//send to shader
+		glm::mat4 normalTrans3 = glm::transpose(glm::inverse(trans3)); //changes
+		glUniformMatrix4fv(normalTransformLoc, 1, GL_FALSE, glm::value_ptr(normalTrans3)); //changes
+		glUniformMatrix4fv(modelTransformLoc, 1, GL_FALSE, glm::value_ptr(trans3));
+
+		glActiveTexture(GL_TEXTURE0);
+		GLuint structureTexture = structure.textures[structure.materials[0].diffuse_texname];
+		glBindTexture(GL_TEXTURE_2D, structureTexture);
+
+
+		// incerement rotation by deltaTime
+		currentTime = glfwGetTime();
+		deltaTime = currentTime - prevTime;
+		xFactor2 += deltaTime * xSpeed2;
+
+		//draw earth
+		glDrawElements(GL_TRIANGLES, structure.numFaces, GL_UNSIGNED_INT, (void*)0);
+
+		//unbindtexture after rendering
+		glBindTexture(GL_TEXTURE_2D, 0);
+
+
+		//////////////BUILDING
+		glBindVertexArray(structure2.vaoId);
+		glUseProgram(shaderProgram); //changes
+
+		// transforms
+		trans4 = glm::mat4(1.0f); // identity
+		//rotates it to the origin point in the world where the sun is also positioned; revolves around the Sun
+		//trans1 = glm::rotate(trans1, glm::radians(xFactor2), glm::vec3(0.0f, 1.0f, 0.0f)); // matrix * rotation_matrix
+		trans4 = glm::rotate(trans, glm::radians(270.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+		//trans1 = glm::rotate(trans, glm::radians(270.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+
+		trans4 = glm::translate(trans4, glm::vec3(90.0f, 200.0f, -55.0f)); // matrix * translate_matrix
+		trans4 = glm::scale(trans4, glm::vec3(0.01f / 1.05, 0.01f / 1.05, 0.01f / 1.05));
+
+		//send to shader
+		glm::mat4 normalTrans4 = glm::transpose(glm::inverse(trans4)); //changes
+		glUniformMatrix4fv(normalTransformLoc, 1, GL_FALSE, glm::value_ptr(normalTrans4)); //changes
+		glUniformMatrix4fv(modelTransformLoc, 1, GL_FALSE, glm::value_ptr(trans4));
+
+		glActiveTexture(GL_TEXTURE0);
+		GLuint structure2Texture = structure2.textures[structure2.materials[0].diffuse_texname];
+		glBindTexture(GL_TEXTURE_2D, structure2Texture);
+
+
+		// incerement rotation by deltaTime
+		currentTime = glfwGetTime();
+		deltaTime = currentTime - prevTime;
+		xFactor2 += deltaTime * xSpeed2;
+
+		//draw earth
+		glDrawElements(GL_TRIANGLES, structure2.numFaces, GL_UNSIGNED_INT, (void*)0);
+
+		//unbindtexture after rendering
+		glBindTexture(GL_TEXTURE_2D, 0);
+
+
+
+
+
+
+
+		//////////////Grocery
+		glBindVertexArray(structure3.vaoId);
+		glUseProgram(shaderProgram); //changes
+
+		// transforms
+		trans5 = glm::mat4(1.0f); // identity
+		//rotates it to the origin point in the world where the sun is also positioned; revolves around the Sun
+		//trans1 = glm::rotate(trans1, glm::radians(xFactor2), glm::vec3(0.0f, 1.0f, 0.0f)); // matrix * rotation_matrix
+		trans5 = glm::rotate(trans, glm::radians(270.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+		//trans1 = glm::rotate(trans, glm::radians(270.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+
+		trans5 = glm::translate(trans5, glm::vec3(45.0f, 175.0f, -89.0f)); // matrix * translate_matrix
+		trans5 = glm::scale(trans5, glm::vec3(0.016f, 0.016f, 0.016f));
+
+		//send to shader
+		glm::mat4 normalTrans5 = glm::transpose(glm::inverse(trans5)); //changes
+		glUniformMatrix4fv(normalTransformLoc, 1, GL_FALSE, glm::value_ptr(normalTrans5)); //changes
+		glUniformMatrix4fv(modelTransformLoc, 1, GL_FALSE, glm::value_ptr(trans5));
+
+		glActiveTexture(GL_TEXTURE0);
+		GLuint structure3Texture = structure3.textures[structure3.materials[0].diffuse_texname];
+		glBindTexture(GL_TEXTURE_2D, structure3Texture);
+
+
+		// incerement rotation by deltaTime
+		currentTime = glfwGetTime();
+		deltaTime = currentTime - prevTime;
+		xFactor2 += deltaTime * xSpeed2;
+
+		//draw earth
+		glDrawElements(GL_TRIANGLES, structure3.numFaces, GL_UNSIGNED_INT, (void*)0);
+
+		//unbindtexture after rendering
+		glBindTexture(GL_TEXTURE_2D, 0);
+
+
+
+
+
+		//////////////Grocery
+		glBindVertexArray(structure4.vaoId);
+		glUseProgram(shaderProgram); //changes
+
+		// transforms
+		trans6 = glm::mat4(1.0f); // identity
+		//rotates it to the origin point in the world where the sun is also positioned; revolves around the Sun
+		//trans1 = glm::rotate(trans1, glm::radians(xFactor2), glm::vec3(0.0f, 1.0f, 0.0f)); // matrix * rotation_matrix
+		trans6 = glm::rotate(trans, glm::radians(270.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+		//trans6 = glm::rotate(trans, glm::radians(270.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+		//trans1 = glm::rotate(trans, glm::radians(270.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+
+		trans6 = glm::translate(trans6, glm::vec3(50.0f, -190.0f, -87.0f)); // matrix * translate_matrix
+		trans6 = glm::scale(trans6, glm::vec3(0.001f * 4, 0.001f * 4, 0.001f * 4));
+
+		//send to shader
+		glm::mat4 normalTrans6 = glm::transpose(glm::inverse(trans6)); //changes
+		glUniformMatrix4fv(normalTransformLoc, 1, GL_FALSE, glm::value_ptr(normalTrans6)); //changes
+		glUniformMatrix4fv(modelTransformLoc, 1, GL_FALSE, glm::value_ptr(trans6));
+
+		glActiveTexture(GL_TEXTURE0);
+		GLuint structure4Texture = structure4.textures[structure4.materials[0].diffuse_texname];
+		glBindTexture(GL_TEXTURE_2D, structure4Texture);
+
+
+		// incerement rotation by deltaTime
+		currentTime = glfwGetTime();
+		deltaTime = currentTime - prevTime;
+		xFactor2 += deltaTime * xSpeed2;
+
+		//draw earth
+		glDrawElements(GL_TRIANGLES, structure4.numFaces, GL_UNSIGNED_INT, (void*)0);
+
+		//unbindtexture after rendering
+		glBindTexture(GL_TEXTURE_2D, 0);
 		//--- stop drawing here ---
 #pragma endregion
 
